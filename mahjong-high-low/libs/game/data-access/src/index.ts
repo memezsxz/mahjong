641ca3010d09ceccb9ba1f0de34c9ff7f7d2acca
meme <mmeme0100@gmail.com>
2026-04-25 01:52:47 +0300

    added the leader board service to query the api

--- mahjong-high-low/libs/game/data-access/src/index.ts
+++ mahjong-high-low/libs/game/data-access/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/data-access/data-access';
+export * from './lib/leaderboard.service';
+export * from './lib/settings.service';
+export * from './lib/tokens';
+export * from './lib/+state/game.store';
--- mahjong-high-low/web/src/app/app.config.ts
+++ mahjong-high-low/web/src/app/app.config.ts
@@ -1,10 +1,16 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners,} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {appRoutes} from './app.routes';
+import {environment} from "../environments/environment";
+import { API_URL } from '@hbg/game-data-access';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(appRoutes),
+        {
+            provide: API_URL,
+            useValue: environment.apiUrl
+        }
+    ],
 };
