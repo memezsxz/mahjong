312d95a46f9231711e1da5e8089c18d8ae35311d
meme <mmeme0100@gmail.com>
2026-04-27 08:43:51 +0300

    added angular/service-worker for caching

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection, isDevMode } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
 
-bootstrapApplication(App, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
+bootstrapApplication(App, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]})
   .catch((err) => console.error(err));
